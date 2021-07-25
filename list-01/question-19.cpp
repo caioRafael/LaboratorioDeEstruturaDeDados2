8eb42a200c2dc80922ddec541ec017c331dc684b
Paulo Lopes <silvavitorlopes711@gmail.com>
2021-07-25 16:13:38 -0300

    feat: Initing question 19

---
+++ list-01/question-19.cpp
@@ -0,0 +1,4 @@
+// Problema 19 Escreva dois programas que recebem como parâmetro três
+// inteiros representando os coeficientes de uma função do segundo grau e execute:
+// a) O teste para saber se essa função tem raízes reais;
+// b) O valor das raízes reais.
