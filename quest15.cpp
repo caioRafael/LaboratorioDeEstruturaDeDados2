17496b87553cd2f774e1b1935b00906285c84023
paulinho <soeupfv@hotmail.com>
2021-07-25 16:06:38 -0300

    feat: quest15 corect

--- quest15.cpp
+++ quest15.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <string.h>
+
+void point (int lado1, int lado2);
+
 int main() {
     int lado1, lado2;
 
     printf("digite nuemro de linhas e numero de colunas\n");
     scanf("%d %d", &lado1, &lado2);
+    point(lado1, lado2);
 
+    return 0;
+}
+
+void point (int lado1, int lado2){
     char caxota[lado1][lado2];
     for(int x = 0; x < lado1; x++){
         for(int y = 0; y < lado2; y++){
@@ -18,7 +26,4 @@ int main() {
         }
         printf("\n");
     }
-  
-
-    return 0;
 }
