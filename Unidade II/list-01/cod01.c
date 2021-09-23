6de521a08b4a6f1dfea8c779187661d4cb9cab29
jonas-henrique <jonashenriquelins@outlook.com>
2021-09-22 21:11:42 -0300

    Feat: cod01

--- Unidade II/list-01/cod01.c
+++ Unidade II/list-01/cod01.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
-#define TAMANHO 8
+#define TAMANHO 32
 typedef struct
 {
     int chave;
 } HASH;
 HASH hash[TAMANHO];
-
+int calc_expoente(int n)
+{
+    if (n != 0 && n != 1)
+    {
+        return 2 * calc_expoente(n - 1);
+    }
+    else if (n == 0)
+    {
+        return 1;
+    }
+    else if (n == 1)
+    {
+        return 2;
+    }
+}
 void inicializa_hash()
 {
 
@@ -20,13 +34,110 @@ int hash_divisao(int chave)
 }
 int hash_dobra(int chave)
 {
-    int total_digitos_bin = 10;
-    int p1 = chave >> total_digitos_bin;
-    int p2 = chave & (TAMANHO - 1);
+    int chave2 = chave;
+    int aux = chave2;
+    int x, tamanho = 0;
+    int j;
+    do
+    {
+        aux = aux / 2;
+        tamanho++;
+    } while (aux != 0);
+
+    int qtd_binarios[tamanho];
+    j = tamanho - 1;
+    //vai de tamanho até o 1, por isso tem o -1 pra ir do tamanho -1 ao 0
+    do
+    {
+        qtd_binarios[j] = chave2 % 2;
+        j--;
+        chave2 = chave2 / 2;
+
+    } while (chave2 != 0);
+
+    int total[10], diferença = 10 - tamanho; //10 digitos cada chave deve ter
+    for (int i = 0; i < diferença; i++)
+    {
+        total[i] = 0;
+    }
+    int inicio = 0;
+    for (int i = diferença; i < 10; i++)
+    {
+        total[i] = qtd_binarios[inicio];
+        inicio++;
+    }
+    //vet[10] preechido
+
+    int p1 = 0, p2 = 0; //metade 1 e metade 2 do vetor de 10 auxsicoes
+                        //primeira metade
+
+    inicio = 4;
+    for (int i = 0; i < 5; i++)
+    {
+        p1 = p1 + (total[i] * calc_expoente(inicio));
+        tamanho++, inicio--;
+    }
+    //segunda metade
+    inicio = 4;
+    for (int i = 5; i < 10; i++)
+    {
+        p2 = p2 + (total[i] * calc_expoente(inicio));
+        tamanho++, inicio--;
+    }
     return (p1 ^ p2);
 }
-int hash_multi(int chave)
+int hash_mult(int chave)
 {
+    //Tamanho hash 32 tem 6 bits,logo pega os 6 do meio
+    int aux = chave * chave, tamanho = 0, j; //recebe chave^2
+    int chave2 = chave * chave;
+
+    do
+    {
+        aux = aux / 2;
+        tamanho++;
+    } while (aux != 0);
+
+    int qtd_binarios[tamanho];
+    j = tamanho - 1;
+    //tamanho-1 pois o vetor inicia do 0
+    do
+    {
+        qtd_binarios[j] = chave2 % 2;
+        j--;
+        chave2 = chave2 / 2;
+
+    } while (chave2 != 0);
+
+    int total[32], diferença = 32 - tamanho; //32 digitos cada chave deve ter
+    //diferença = total de numeros que falta pra preencher as 32 posicoes da chave
+    for (int i = 0; i < diferença; i++)
+    {
+        total[i] = 0;
+    }
+    int inicio = 0;
+    for (int i = diferença; i < 32; i++)
+    {
+        total[i] = qtd_binarios[inicio];
+        inicio++;
+    }
+    int meio[6];
+    int l = 13; //comeca na posição 14, que no vetor é 13
+    for (int i = 0; i < 6; i++)
+    {
+        meio[i] = total[l];
+        l++;
+    }
+
+    int numero_meio = 0;
+
+    inicio = 5;
+    for (int i = 0; i < 6; i++)
+    {
+        numero_meio = numero_meio + (meio[i] * calc_expoente(inicio));
+        tamanho++, inicio--;
+    }
+    return numero_meio;
 }
 HASH preencher()
 {
@@ -45,7 +156,7 @@ void inserir(int metodo)
     }
     else if (metodo == 2)
     {
-        //nada
+        resto = hash_mult(h.chave);
     }
     else if (metodo == 3)
     {
@@ -55,6 +166,7 @@ void inserir(int metodo)
     {
         hash[resto].chave = h.chave;
     }
+    printf("\n");
 }
 void mostrar()
 {
@@ -63,7 +175,7 @@ void mostrar()
     {
         if (hash[i].chave != -1)
         {
-            printf("pos: %d\tchave: %d\n", i, hash[i].chave);
+            printf("pos: %d\t|chave: %d\n", i, hash[i].chave);
         }
         else
         {
@@ -75,10 +187,13 @@ void mostrar()
 void menu()
 {
     printf("Tamanho do vetor: %d\n\n", TAMANHO);
+    printf("-----------------------------------\n");
+
     printf("1 - Funcao dobra\n");
     printf("2 - Funcao Multiplicacao\n");
     printf("3 - funcao divisao\n");
-    printf("0 - SAIR\n");
+    printf("0 ou >3  - SAIR\n");
+    printf("-----------------------------------\n");
 }
 void analisa(int opc)
 {
@@ -96,6 +211,7 @@ void analisa(int opc)
         }
 
     } while (continuar == 1);
+    printf("\n");
 }
 int main()
 {
@@ -107,16 +223,19 @@ int main()
     if (opc == 1)
     {
         analisa(opc);
+        printf("FUNCAO HASH DOBRA:\n");
         mostrar();
     }
     else if (opc == 2)
     {
         analisa(opc);
+        printf("FUNCAO HASH MULTIPLICACAO:\n");
         mostrar();
     }
     else if (opc == 3)
     {
         analisa(opc);
+        printf("FUNCAO HASH DIVISAO:\n");
         mostrar();
     }
     else
